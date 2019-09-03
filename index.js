a1079ac5cc5e018532edfa50f0925f15f88720e9
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-03 15:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+<<<<<<< HEAD
+=======
 
-var topic = "space";
+var topic = "Let's talk about space";
+>>>>>>> 8039530aec13c327fee31e88791fe66e045aac0c
 
+
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+>>>>>>> 8039530aec13c327fee31e88791fe66e045aac0c
